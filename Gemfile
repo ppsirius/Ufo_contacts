ff8204c968f9cadb7f812bf9d531d845c2c3619b
Piotr Pupczyk <piotrek@jakbyco.com>
2014-08-26 15:11:41 +0200

    few change on view

--- Gemfile
+++ Gemfile
@@ -30,7 +30,7 @@ gem 'spring',        group: :development
 
 gem 'devise'
 
-
+gem 'kaminari'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/all_controller.rb
+++ app/controllers/all_controller.rb
@@ -1,6 +1,8 @@
 class AllController < ApplicationController
 	def index
-		@city_all = City.all
-		@contact_all = Contact.all
+# @cont = Contact.all
+
+	@cities = City.joins(:contacts).group(Contact.arel_table[:city_id]).order(Contact.arel_table[:city_id].count).reverse	
+@cities = Kaminari.paginate_array(@cities).page(params[:page]).per(10) 
 	end
 end
