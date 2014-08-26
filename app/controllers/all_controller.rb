47fbe9c21af8ef8af82c5bd23ec70534be01b2bc
Piotr Pupczyk <piotrek@jakbyco.com>
2014-08-26 17:48:45 +0200

    old version

--- app/controllers/all_controller.rb
+++ app/controllers/all_controller.rb
@@ -1,8 +1,6 @@
 class AllController < ApplicationController
+
 	def index
-# @cont = Contact.all
-
-	@cities = City.joins(:contacts).group(Contact.arel_table[:city_id]).order(Contact.arel_table[:city_id].count).reverse	
-
+		@cities = City.joins(:contacts).group(Contact.arel_table[:city_id]).order(Contact.arel_table[:city_id].count).reverse	
 	end
 end
